1fc9c7116bed718d3e88f076e644b2b4a2792c7b
Matthew Kuntz <matt@mfkuntz.com>
2015-09-19 23:24:42 -0400

    Delete data older than 1 day when we insert new data.

--- app/src/main/java/com/mfkuntz/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/mfkuntz/sunshine/sync/SunshineSyncAdapter.java
@@ -39,6 +39,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.Vector;
 
 
@@ -473,6 +474,11 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                 ContentValues[] cvArray = new ContentValues[cVVector.size()];
                 cVVector.toArray(cvArray);
                 inserted = getContext().getContentResolver().bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, cvArray);
+
+                //delete data older than 1 day
+                getContext().getContentResolver().delete(WeatherContract.WeatherEntry.CONTENT_URI,
+                        WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
+                        new String[] {Long.toString(dayTime.setJulianDay(julianStartDay - 1))});
 
                 notifyWeather();
             }
